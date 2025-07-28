b5f6ecb06ece2981daf6c694b50a66b05d9bf5b7
juliaahn <jra8606@nyu.edu>
2025-07-28 16:52:54 -0400

    created holdings component

--- UI/portfolio-ui/src/app/components/holdings/holdings.component.ts
+++ UI/portfolio-ui/src/app/components/holdings/holdings.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from '../../services/portfolio.service';
+import { Holding, PortfolioData } from '../../models/portfolio.model';
 
 @Component({
   selector: 'app-holdings',
@@ -7,4 +9,38 @@ import { Component } from '@angular/core';
 })
 export class HoldingsComponent {
 
+  portfolio: PortfolioData | null = null;
+  holdings : Holding[] = [];
+  cashBalance: number = 0;
+  totalMarketValue: number = 0;
+
+  constructor(private portfolioService: PortfolioService) { }
+
+  ngOnInit(): void {
+    this.portfolioService.portfolio$.subscribe(data => {
+      if (data){
+        this.portfolio = data;
+        this.holdings = data.holdings;
+        this.cashBalance = data.totals.cash_balance;
+        this.totalMarketValue = data.totals.total_market_value;
+        console.log(data);
+      }
+    })
+  }
+
+  getNetWorth(): number {
+    return this.totalMarketValue + this.cashBalance;
+  }
+
+  getCash(): number {
+    return this.cashBalance;
+  }
+
+  getTotalMarketValue(): number{
+    return this.totalMarketValue;
+  }
+
+  getTotalInvested(holding: Holding): number {
+    return holding.quantity * holding.average_cost;
+  }
 }
